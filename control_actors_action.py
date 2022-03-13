6fe437129819455a76f951e08f456dfb9f53e9c8
slowedpepper7 <97586351+slowedpepper7@users.noreply.github.com>
2022-03-12 17:16:16 -0700

    Update control_actors_action.py

--- control_actors_action.py
+++ control_actors_action.py
@@ -1,7 +1,9 @@
 import constants
 from action import Action
 from point import Point
+import random
 
+last_key = ''
 
 class ControlActorsAction(Action):
     """
@@ -33,36 +35,94 @@ class ControlActorsAction(Action):
             script (Script): The script of Actions in the game.
         """
         # left
+        score = cast.get_first_actor("scores")
+        food = cast.get_first_actor("foods")
+        snake = cast.get_first_actor("snakes")
+        snake2 = cast.get_second_actor("snakes")
+        head = snake.get_head()
+        points = food.get_points()
+        global last_key
+        # snake.grow_tail(points)
+        # snake2.grow_tail(points)
+        number = 1
+        
         if self._keyboard_service.is_key_down('a'):
             self._direction = Point(-constants.CELL_SIZE, 0)
+            if last_key != 'a':
+                grow = random.randint(0,3)
+                if grow == 2:
+                    snake.grow_tail(points)
+                    last_key = 'a'
+            
         
         # right
         if self._keyboard_service.is_key_down('d'):
             self._direction = Point(constants.CELL_SIZE, 0)
+
+            if last_key != 'd':
+                grow = random.randint(0,3)
+                if grow == 2:
+                    snake.grow_tail(points)
+                    last_key = 'd'
         
         # up
         if self._keyboard_service.is_key_down('w'):
             self._direction = Point(0, -constants.CELL_SIZE)
+
+            if last_key != 'w':
+                grow = random.randint(0,4)
+                if grow == 2:
+                    snake.grow_tail(points)
+                    last_key = 'w'
         
         # down
         if self._keyboard_service.is_key_down('s'):
             self._direction = Point(0, constants.CELL_SIZE)
 
+            if last_key != 's':
+                grow = random.randint(0,4)
+                if grow == 2:
+                    snake.grow_tail(points)
+                    last_key = 's'
+
         # left
         if self._keyboard_service.is_key_down('j'):
             self._direction2 = Point(-constants.CELL_SIZE, 0)
+
+            if last_key != 'j':
+                grow = random.randint(0,3)
+                if grow == 2:
+                    snake2.grow_tail2(points)
+                    last_key = 'j'
         
         # right
         if self._keyboard_service.is_key_down('l'):
             self._direction2 = Point(constants.CELL_SIZE, 0)
+            if last_key != 'l':
+                grow = random.randint(0,3)
+                if grow == 2:
+                    snake2.grow_tail2(points)
+                    last_key = 'l'
         
         # up
         if self._keyboard_service.is_key_down('i'):
             self._direction2 = Point(0, -constants.CELL_SIZE)
+
+            if last_key != 'i':
+                grow = random.randint(0,4)
+                if grow == 2:
+                    snake2.grow_tail2(points)
+                    last_key = 'i'
         
         # down
         if self._keyboard_service.is_key_down('k'):
             self._direction2 = Point(0, constants.CELL_SIZE)
+
+            if last_key != 'k':
+                grow = random.randint(0,4)
+                if grow == 2:
+                    snake2.grow_tail2(points)
+                    last_key = 'k'
         
         snake = cast.get_first_actor("snakes")
         snake.turn_head(self._direction)
